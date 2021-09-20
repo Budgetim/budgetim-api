01982fd73b64732ade13f2359353ec9444ac9484
Виталий Павленко <pavlenko.v.ale@sberbank.ru>
2021-09-20 17:53:58 +0300

    fix

--- dist/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logSession = void 0;
-const logSession = (req, res, next) => {
-    next();
-};
-exports.logSession = logSession;
-//# sourceMappingURL=index.js.map
