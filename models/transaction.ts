1abc520ff26070225e9f246631a8bc504f35bac9
Виталий Павленко <pavlenko.v.ale@sberbank.ru>
2021-09-27 00:52:09 +0300

    добавил id категории в ответ

--- models/transaction.ts
+++ models/transaction.ts
@@ -1,18 +1,17 @@
 import { pool } from './db';
-import { convertTransaction } from './convertTransaction';
 
 export default class Transaction {
   static async get() {
     const transactions: any = await pool.query(`
-      SELECT transaction.id, transaction.title, category.title AS category, transaction.price, transaction.date FROM transaction
+      SELECT transaction.id, transaction.title, category.title AS category, transaction.category_id AS categoryId, transaction.price, transaction.date FROM transaction
       INNER JOIN category ON transaction.category_id = category.id
     `);
-    return transactions[0].map(transaction => convertTransaction(transaction));
+    return transactions[0];
   }
 
   static async getById(id: number) {
     const transaction = await pool.query(`
-    SELECT transaction.id, transaction.title, category.title AS category, transaction.price, transaction.date FROM transaction 
+    SELECT transaction.id, transaction.title, category.title AS category, transaction.category_id AS categoryId, transaction.price, transaction.date FROM transaction 
     INNER JOIN category ON transaction.category_id = category.id
     WHERE ?? = ?`,
       ['transaction.id', id]
@@ -26,7 +25,7 @@ export default class Transaction {
       [title, categoryId, price, date, 1],
     );
     const transaction = await this.getById(res[0].insertId);
-    return convertTransaction(transaction);
+    return transaction;
   }
 
   static async delete(id: number) {
@@ -37,6 +36,6 @@ export default class Transaction {
   static async edit({ id, title, categoryId, price, date }: { id: number; title: string; categoryId: number; price: number; date: string; }) {
     await pool.query('UPDATE transaction SET title = ?, category_id = ?, price = ?, date = ? WHERE id = ?', [title, categoryId, price, date, id],);
     const transaction = await this.getById(id);
-    return convertTransaction(transaction);
+    return transaction;
   }
 }
