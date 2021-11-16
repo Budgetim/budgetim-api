ea421f8ee96c79e2a326b1a3859bd238d08c05ed
Виталий Павленко <pavlenko.v.ale@sberbank.ru>
2021-11-16 18:03:36 +0300

    fix

--- controllers/currency/currency.ts
+++ controllers/currency/currency.ts
@@ -3,6 +3,9 @@ import { Response, Request } from 'express';
 import Currency from '../../models/currency';
 
 export const getCurrencies = async (req: Request, res: Response) => {
-  const categories = await Currency.get();
-  res.send(categories);
+  //const categories = await Currency.get();
+  //res.send(categories);
+  res.send({
+    secret: process.env.BUDGETIM_SECRET,
+  })
 };
